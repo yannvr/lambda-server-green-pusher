c3b583fdc6a4701e05bd85f5a8719643a94a61de
Yann VR <yannvr@gmail.com>
2020-10-14 23:26:17 +0100

    chore(docs) add cron files

---
+++ docs/cron-files/send-test-broadcast.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+date=`date "+%d-%m-%y%H:%M:%S"`
+echo "${date}: Pushing notif schedule" >> /var/log/cron.greenlife
+
+# Warm up function
+curl -H "cypress: 42" -H "meonly: 1" https://greenlife-quote-pusher.yannvr.now.sh/api/warmup
+
+if [ $? -ne 0 ] ; then
+    # Re execute lambda
+    echo "${date}: FAILURE warmup (RETRY)" >> /var/log/cron.greenlife
+    /bin/sh /home/ubuntu/GreenLife/send-test-broadcast.sh
+    exit
+fi
+
+sleep 1
+
+# Execute function (hopefully)
+curl -H "cypress: 42" -H "meonly: 1" https://greenlife-quote-pusher.yannvr.now.sh/api/broadcast-quote
+
+if [ $? -ne 0 ] ; then
+    # Re execute lambda
+    echo "${date}: FAILURE broadcast (RETRY)" >> /var/log/cron.greenlife
+    /bin/sh /home/ubuntu/GreenLife/send-test-broadcast.sh
+    exit
+fi
+
+echo "${date}: OK" >> /var/log/cron.greenlife
